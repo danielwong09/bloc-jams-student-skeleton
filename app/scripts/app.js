3d1e57d90b808c49499a1c66c6ad7e30fffe864a
Daniel Wong <daniel.wong09@gmail.com>
2015-02-16 12:00:59 -0500

    Converted Bloc Jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,25 +3,39 @@
 // require('./album');
 // require('./profile');
 
+blocJams = angular.module('BlocJams', ['ui.router']);
 
-angular.module('BlocJams',[]).controller('Landing.controller', ['$scope',function($scope){
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+}]);
+ 
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
+
   $scope.subText = 'Turn the music up!';
 
   $scope.foo = function(){
-
     $scope.subText += '!';
   };
 
-   $scope.albumURLs = [
-     '/images/album-placeholders/album-1.jpg',
-     '/images/album-placeholders/album-2.jpg',
-     '/images/album-placeholders/album-3.jpg',
-     '/images/album-placeholders/album-4.jpg',
-     '/images/album-placeholders/album-5.jpg',
-     '/images/album-placeholders/album-6.jpg',
-     '/images/album-placeholders/album-7.jpg',
-     '/images/album-placeholders/album-8.jpg',
-     '/images/album-placeholders/album-9.jpg',
-   ];
+  $scope.albumURLs = [
+   '/images/album-placeholders/album-1.jpg',
+   '/images/album-placeholders/album-2.jpg',
+   '/images/album-placeholders/album-3.jpg',
+   '/images/album-placeholders/album-4.jpg',
+   '/images/album-placeholders/album-5.jpg',
+   '/images/album-placeholders/album-6.jpg',
+   '/images/album-placeholders/album-7.jpg',
+   '/images/album-placeholders/album-8.jpg',
+   '/images/album-placeholders/album-9.jpg',
+  ];
 
 }]);
