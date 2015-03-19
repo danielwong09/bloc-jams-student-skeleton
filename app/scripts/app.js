70a19e49d6472dac2b0a031c71334494dc180151
Daniel Wong <daniel.wong09@gmail.com>
2015-03-19 05:14:54 +0000

    Changed slider to update (music location) data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@ var albumPicasso = {
  year: '1881',
  albumArtUrl: '/images/album-placeholder.png',
  songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
    ]
 };
 
@@ -179,6 +179,14 @@ blocJams.service('SongPlayer', function() {
       this.playing = false;
       this.currentSoundFile.pause();
     },
+    seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(this.currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         this.currentSoundFile.setTime(time);
+       }
+     },
+    
     setSong: function(album, song) {
       if (this.currentSoundFile) {
         this.currentSoundFile.stop();
@@ -225,21 +233,51 @@ blocJams.directive('slider', ['$document', function($document){
     offsetXPercent = Math.min(1, offsetXPercent);
     return offsetXPercent;
   };
+  
+  //Note, bloc has this after the link function?
+  var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+ 
 
-  function link(scope, element, attrs) {
+  function link(scope, element, attributes) {
     scope.value = 0;
     scope.max = 100;
     var $seekBar = $(element);
     
+    console.log(attributes);
+    
+    attributes.$observe('value', function(newValue) {
+      scope.value = numberFromValue(newValue, 0);
+    });
+
+    attributes.$observe('max', function(newValue) {
+      scope.max = numberFromValue(newValue, 100) || 100;
+    });
+    
     scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+        notifyCallback(scope.value);
        }
     
     var percentString = function () {
-      var percent = Number(scope.value) / Number(scope.max) * 100;
+      var value = scope.value || 0;
+      var max = scope.max || 100;
+      percent = value / max * 100;
       return percent + "%";
     }
+    
     
     scope.fillStyle = function(){
       return {width: percentString()};
@@ -254,6 +292,7 @@ blocJams.directive('slider', ['$document', function($document){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+             notifyCallback(scope.value);
            });
          });
  
@@ -263,6 +302,12 @@ blocJams.directive('slider', ['$document', function($document){
            $document.unbind('mouseup.thumb');
          });
        };
+    
+    var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
+       };
 
   };//end link function
 
@@ -271,6 +316,8 @@ blocJams.directive('slider', ['$document', function($document){
     restrict: 'E',
     replace: true,
     templateUrl: '/templates/directives/slider.html',
-    scope:{}
+    scope: {
+      onChange: '&'
+    },
   }
 }]);//end directive
